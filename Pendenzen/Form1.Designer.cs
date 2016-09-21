aed3d3fdf4815ab2e2df4efbea33898774c1dbe0
Christian Seiler <christian@christianseiler.ch>
2016-09-21 15:33:10 +0200

    Initial

---
+++ Pendenzen/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Pendenzen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.firmaComboBox = new System.Windows.Forms.ComboBox();
+            this.firmaLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name: Bob Meierhans";
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = global::Pendenzen.Properties.Resources.logo;
+            this.logoPictureBox.Location = new System.Drawing.Point(13, 13);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(187, 50);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoPictureBox.TabIndex = 1;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // firmaComboBox
+            // 
+            this.firmaComboBox.FormattingEnabled = true;
+            this.firmaComboBox.Location = new System.Drawing.Point(13, 107);
+            this.firmaComboBox.Name = "firmaComboBox";
+            this.firmaComboBox.Size = new System.Drawing.Size(121, 21);
+            this.firmaComboBox.TabIndex = 2;
+            // 
+            // firmaLabel
+            // 
+            this.firmaLabel.AutoSize = true;
+            this.firmaLabel.Location = new System.Drawing.Point(15, 91);
+            this.firmaLabel.Name = "firmaLabel";
+            this.firmaLabel.Size = new System.Drawing.Size(32, 13);
+            this.firmaLabel.TabIndex = 3;
+            this.firmaLabel.Text = "Firma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 593);
+            this.Controls.Add(this.firmaLabel);
+            this.Controls.Add(this.firmaComboBox);
+            this.Controls.Add(this.logoPictureBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.ComboBox firmaComboBox;
+        private System.Windows.Forms.Label firmaLabel;
+    }
+}
+
