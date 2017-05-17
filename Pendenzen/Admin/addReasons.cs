05665692641fcbf32bb329d7f9fda497c136736e
Christian Seiler <christian@christianseiler.ch>
2017-05-17 12:21:37 +0200

    Initial Commit "addReasons" Class

---
+++ Pendenzen/Admin/addReasons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pendenzen.Admin
+{
+    public partial class addReasons : Form
+    {
+        public addReasons()
+        {
+            InitializeComponent();
+        }
+    }
+}
